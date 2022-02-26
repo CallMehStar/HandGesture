8c73e4b778c8bb7f9e5216ebfa80720a135b27d0
CallMehStar <88277772+CallMehStar@users.noreply.github.com>
2022-02-26 20:31:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-prediction_1 = "";
+prediction_1 = ""
 
 Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
-    png_quailty:90
-});
+    png_quality:90
+  });
 
 camera = document.getElementById("camera");
 
-Webcam.attach( '#camera' );
+Webcam.attach('#camera');
 
 function take_snapshot() {
     Webcam.snap(function(data_uri){
@@ -17,9 +17,9 @@ function take_snapshot() {
     });
 }
 
-console.log('ml5 version', ml5.version);
+console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/YGeYuAMV3/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/b1f9n5BPh/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log('Model Loaded!');
@@ -30,4 +30,35 @@ function speak() {
     speak_data_1 = "The first prediction is " + prediction_1;
     var utterThis = new SpeechSynthesisUtterance(speak_data_1);
     synth.speak(utterThis);
+}
+
+function check () {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }   else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+        speak();
+        if(results[0].label == "Okay Sign") {
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Thumbs Up") {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Index Pointing Up") {
+            document.getElementById("update_emoji").innerHTML = "&#9757;";
+        }
+        if(results[0].label == "Sign of the Horns") {
+            document.getElementById("update_emoji").innerHTML = "&#129304;";
+        }
+        if(results[0].label == "Victory Sign") {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+    }
 }
